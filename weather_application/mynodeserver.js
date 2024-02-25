f9a4bcfada620c7fd79c5404896fbdfe7416d97c
osjohn01 <osjohn01@gmail.com>
2024-02-25 16:17:20 +0530

    commit

--- weather_application/mynodeserver.js
+++ weather_application/mynodeserver.js
@@ -3,26 +3,29 @@ const axios = require('axios');
 
 const app = express();
 const port = 8080;
-app.get('/temperature/:location_code', async function(request, response) {
-    const varlocation = request.params.location_code;
+app.get('/:location', async function(request, response) {
+    const varlocation = request.params.location;
     try {
         const options = {
             method: 'GET',
-            url: 'https://open-weather13.p.rapidapi.com/city/landon',
+            url: `https://open-weather13.p.rapidapi.com/city/${varlocation}`,
             headers: {
                 'X-RapidAPI-Key': 'bf5e9bdeb0mshd393abff3c88a2dp1c9ff9jsnb3e9b0598e0d',
                 'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
             }
         };
-        const apiResponse = await axios.request(options);
+        const apiResponse = await axios.request(options)
         const data = apiResponse.data
-        console.log(data);
-        response.send(data.coord);
+        const location = data.name
+        console.log("Location is : ", location);
+        console.log("TEMPERATURE --> " ,data.main.temp)
+        response.send(`TEMPERATURE --> ${data.main.temp}`);
     } catch (error) {
         console.error('Error fetching weather data:', error);
         response.status(500).send('Error fetching weather data');
     }
 });
+
 
 let server = app.listen(port, function() {
     console.log(`Listening on URL http://localhost:${port}`);
